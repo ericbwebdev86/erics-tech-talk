dfea0ddbed314372b79e33019ee8448fbc63e72e
unknown <ericbwebdev86@gmail.com>
2022-03-03 09:54:20 -0600

    created route for authentication

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -46,7 +46,26 @@ router.post('/', (req,res) => {
         res.status(500).json(err);
     })
 });
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({ message: 'No user with that username.' });
+            return;
+        }
 
+        //verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password.'});
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!'});
+    });
+});
 //PUT single
 router.put('/:id', (req,res) => {
     User.update(req.body, {
