84632a27e6ce03e6f93973b67e9157ddd97b77a1
unknown <ericbwebdev86@gmail.com>
2022-03-04 14:46:20 -0600

    dashboard view and get route created

--- public/js/login.js
+++ public/js/login.js
@@ -40,7 +40,7 @@ async function loginHandler(event) {
         });
         if (response.ok) {
             console.log('success');
-            document.location.replace('/')
+            document.location.replace('/dashboard')
         } else {
             alert(response.statusText);
         }
